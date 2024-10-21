59433fbc729746705c46c7e8d735c7e86c8428e4
Wolfyxon <wolfyxon@gmail.com>
2024-10-21 16:01:26 +0200

    Init 16.10.2024

---
+++ highSchool/3/ProgramowanieObiektowe/16.10.2024/main.py
@@ -0,0 +1,40 @@
+baza_kontaktow = {
+    # name: number
+}
+
+class Option:
+  def __init__(self, id: str, display: str):
+    self.id = id
+    self.display = display
+
+options = [
+    Option("new", "Dodaj nowy kontakt"),
+    Option("search", "Wyszukaj kontakt"),
+    Option("update", "Zaktualizuj numer telefonu"),
+    Option("delete", "Usuń kontakt"),
+    Option("exit", "Zakończ program")
+]
+
+def main():
+  print()
+  for i in range(len(options)):
+    print(f"{i + 1}. {options[i].display}")
+
+  try:
+    opt_id = int(input("Wybierz opcję: "))
+
+    if opt_id < 1 or opt_id > len(options):
+      print("Nieznana opcja")
+      return main()
+
+    match options[opt_id - 1].id:
+      case "exit":
+        print("żegnaj")
+        return
+
+  except:
+    print("Niepoprawna liczba")
+
+  main()
+
+main()
