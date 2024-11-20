b450b25a56d34e8519be1735955055a6930bfc4f
Wolfyxon <wolfyxon@gmail.com>
2024-11-20 14:37:11 +0100

    Added epic 20.11.2024

---
+++ highSchool/3/ProgramowanieObiektowe/20.11.2024/recru_tree.py
@@ -0,0 +1,52 @@
+import random
+import re
+
+width = 20
+init_w = 4
+leaves_height = width - 1
+
+def ansi(code: int) -> str:
+    return f"\033[0;{code}m"
+
+def style(s: str, code: int) -> str:
+    return ansi(code) + s + ansi(0)
+
+def unstyle(s: str) -> str:
+    return re.sub("(\x9B|\x1B\[)[0-?]*[ -\/]*[@-~]", "", s)
+
+def printCen(s: str):
+    space = " " * ((width - len(unstyle(s))) // 2)
+
+    print(space + s)
+
+def leaves(w: int = 1, max_w: int = init_w):
+    if w > max_w:
+        w = 4
+        max_w += 5
+
+    if max_w > width:
+        return
+    
+    row = ""
+
+    for i in range(w):
+        if i == max_w // init_w:
+            row += "8"
+        else:
+            if random.randint(0, 8) == 0:
+                row += style("O", random.randint(30, 36))
+            else:
+                row += style("*", 32)
+
+    printCen(row)
+    leaves(w + 2, max_w)
+
+def trunk(i: int = 1):
+    if i > 2: return
+
+    printCen(style("||", 31))
+    trunk(i + 1)
+
+printCen(style("X", 33))
+leaves()
+trunk()
