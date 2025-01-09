b9ff954cc413eab99ed61425fe5fb5f4ffe1ddef
Wolfyxon <wolfyxon@gmail.com>
2025-01-09 15:39:32 +0100

    Using a dict instead

--- highSchool/3/PracowniaProgramowaniaObiektowego/09.01.2025/main.py
+++ highSchool/3/PracowniaProgramowaniaObiektowego/09.01.2025/main.py
@@ -1,8 +1,22 @@
-roman_nums = [
-    "I", "V", "X", "L", "C", "D", "M"
-]
+#"I", "V", "X", "L", "C", "D", "M"
+roman_nums = {
+    1: "I",
+    4: "IV",
+    5: "V",
+    "9": "IX",
+    10: "X",
+    40: "XL",
+    50: "L",
+    90: "XC",
+    100: "C",
+    400: "CD",
+    500: "D",
+    900: "CM",
+    1000: "M"
+}
 
 arab = int(input("Podaj liczbę arabską: "))
+roman = ""
 
 if arab <= 0:
     print("Liczba musi być wieksza od 0")
