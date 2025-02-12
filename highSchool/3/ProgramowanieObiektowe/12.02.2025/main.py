19c60f4c54ea507858b0632c00d31beb1e1e3b1b
Wolfyxon <wolfyxon@gmail.com>
2025-02-12 14:50:53 +0100

    Added everything else

--- highSchool/3/ProgramowanieObiektowe/12.02.2025/main.py
+++ highSchool/3/ProgramowanieObiektowe/12.02.2025/main.py
@@ -18,6 +18,9 @@ class User:
         self.name = name
         self.balance = balance
 
+    def __str__(self) -> str:
+        return f"{self.name} ({self.balance} zł)"
+
     def kup_bilet(self, movie: Movie):
         if movie.available_tickets <= 0:
             print("Bilety wyprzedane")
@@ -41,3 +44,74 @@ movies = [
     Movie("Kopaćrzemiosło Film", 2, 35),
     Movie("Ryba", 17, 17)
 ]
+
+def print_list(ls: list):
+    for i in range(len(ls)):
+        print(f" {i + 1}. {ls[i]}")
+
+def query_list_menu(ls: list):
+    print_list(ls)
+    
+    return query_list(ls)
+
+def query_list(ls: list):
+    try:
+        idx = int(input("(-1 by anulować) > "))
+
+        if idx == -1: 
+            return
+
+        if len(movies) < idx or idx <= 0:
+            print("Nieznany")
+            return query_list(ls)
+
+        return ls[idx - 1]
+
+    except ValueError:
+        print("Niepoprawna liczba")
+        return query_list(ls)
+
+def query_user() -> User:
+    return query_list_menu(users)
+
+def query_movie() -> Movie:
+    return query_list_menu(movies)
+
+def main():
+    print("Witaj w kinie")
+    print("Co chcesz zrobić?")
+    
+    print("1. Kup bilet")
+    print("2. Wyświetl filmy")
+    print("3. Wyświetl urzytkowników")
+    print("4. Wyjdź")
+
+    query_main()
+
+def query_main():
+    match input("> "):
+        case "1":
+            buy()
+        case "2":
+            print_list(movies)
+        case "3":
+            print_list(users)
+        case "4":
+            return
+
+    main()
+
+def buy():
+    print("Wybierz film:")
+
+    movie = query_movie()
+    if not movie: return
+
+    print("Wybierz użytkownika:")
+
+    user = query_user()
+    if not user: return
+
+    user.kup_bilet(movie)
+
+main()
