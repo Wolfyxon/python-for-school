2816316b3e85aaa0d5358da89f586c22d32023f1
Wolfyxon <wolfyxon@gmail.com>
2025-02-12 14:31:27 +0100

    Impl classes

--- highSchool/3/ProgramowanieObiektowe/12.02.2025/main.py
+++ highSchool/3/ProgramowanieObiektowe/12.02.2025/main.py
@@ -8,9 +8,24 @@ class Movie:
     def __str__(self) -> str:
         return f"{self.title} ({self.available_tickets} / {self.total_tickets}) {self.ticket_cost} zł"
 
+    def zarezerwuj_miejsce(self):
+        assert self.available_tickets > 0, "Sold out"
+
+        self.available_tickets -= 1
+
 class User:
     def __init__(self, name: str, balance: float):
         self.name = name
         self.balance = balance
 
-    
+    def kup_bilet(self, movie: Movie):
+        if movie.available_tickets <= 0:
+            print("Bilety wyprzedane")
+            return
+        
+        if self.balance < movie.ticket_cost:
+            print("Brak wystarczających środków")
+            return
+
+        movie.zarezerwuj_miejsce()
+        self.balance -= movie.ticket_cost
