9585aed2fe02cf8d80071b763b61bbcf71387089
Wolfyxon <wolfyxon@gmail.com>
2025-03-05 14:39:05 +0100

    Adding methods

--- highSchool/3/ProgramowanieObiektowe/05.03.2025/zadanie3/Terminal.py
+++ highSchool/3/ProgramowanieObiektowe/05.03.2025/zadanie3/Terminal.py
@@ -1,6 +1,18 @@
+from Flight import *
+from Passenger import *
+from Aircraft import *
+
 class Terminal:
     def __init__(self):
         self.flights = []
         self.passenger = []
-        self.flights = []
         self.aircrafts = []
+
+    def add_flight(self, f: Flight): 
+        self.flights.append(f)
+        
+    def add_passenger(self, p: Passenger):
+        self.passengers.append(p)
+
+    def add_aircraft(self, a: Aircraft):
+        self.aircrafts.append(a)
