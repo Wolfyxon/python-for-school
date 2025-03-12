a2ae14e42a98a2c55faacf4a1b9edc3ffb04d097
Wolfyxon <wolfyxon@gmail.com>
2025-03-12 12:56:32 +0100

    Added query passenger

--- highSchool/3/ProgramowanieObiektowe/05.03.2025/zadanie3/Terminal.py
+++ highSchool/3/ProgramowanieObiektowe/05.03.2025/zadanie3/Terminal.py
@@ -1,6 +1,7 @@
 from Flight import *
 from Passenger import *
 from Aircraft import *
+from cli import *
 
 class Terminal:
     def __init__(self, flights: list[Flight], passengers: list[Passenger], aircrafts: list[Aircraft]):
@@ -16,3 +17,28 @@ class Terminal:
 
     def add_aircraft(self, a: Aircraft):
         self.aircrafts.append(a)
+
+    def query_passenger(self) -> Passenger:
+        options = []
+
+        for i in self.passengers:
+            options.append(Option(str(i), i))
+
+        options.append(Option("< Nowy >", "new"))
+        res = query_option(options)
+
+        if not res: 
+            return
+
+        if res == "new":
+            name1 = input("Podaj imię: ")
+            name2 = input("Podaj nazwisko: ")
+            doc_id = input_int("Podaj nr dokumentu: ")
+            ticket_class = input_int("Podaj klasę biletu: ")
+
+            p = Passenger(name1, name2, doc_id, ticket_class)
+            self.add_passenger(p)
+
+            return p
+        
+        return res
