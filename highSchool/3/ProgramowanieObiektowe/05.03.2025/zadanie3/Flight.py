179f68166a8f893da514c8897b717d074821bccb
Wolfyxon <wolfyxon@gmail.com>
2025-03-12 14:25:32 +0100

    Flights require aircrafts

--- highSchool/3/ProgramowanieObiektowe/05.03.2025/zadanie3/Flight.py
+++ highSchool/3/ProgramowanieObiektowe/05.03.2025/zadanie3/Flight.py
@@ -1,15 +1,18 @@
 from datetime import datetime
+from Aircraft import *
 
 class Seat:
     def __init__(self, id: int):
         self.id = id
 
 class Flight:
-    def __init__(self, id: int, course: str, departure: datetime, arrival: datetime):
+    def __init__(self, id: int, course: str, departure: datetime, arrival: datetime, aircraft: Aircraft):
         self.id = id
         self.course = course
         self.departure = departure
         self.arrival = arrival
+        self.aircraft = aircraft
+
         self.occupied_seats = []
 
     def get_seat(self, id: int) -> Seat:
