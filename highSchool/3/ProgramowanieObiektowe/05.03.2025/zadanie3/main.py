6ece6b70c22da3c686ea61fcdc1e6235df0caf20
Wolfyxon <wolfyxon@gmail.com>
2025-03-12 14:42:44 +0100

    Implemented reserving

--- highSchool/3/ProgramowanieObiektowe/05.03.2025/zadanie3/main.py
+++ highSchool/3/ProgramowanieObiektowe/05.03.2025/zadanie3/main.py
@@ -3,30 +3,49 @@ from Terminal import *
 from cli import *
 from datetime import datetime
 
+plane1 = Aircraft("Boeing 737", "airbus", 20)
+plane2 = Aircraft("Boeing 707", "airbus", 20)
 
 airport = Airport([
     Terminal([
         Flight(
             1234, "Warszawa -> Bruksela", 
             datetime(2025, 6, 17, 11, 30), 
-            datetime(2025, 6, 17, 18, 40)
+            datetime(2025, 6, 17, 18, 40),
+            plane1
         )
     ], [], [
-        Aircraft("Boeing 737", "airbus", 20)
+       plane1
     ]),
     Terminal([
             Flight(
             4321, "Warszawa -> Waszyngton", 
             datetime(2025, 4, 5, 15, 00), 
-            datetime(2025, 5, 5, 6, 10)
+            datetime(2025, 5, 5, 6, 10),
+            plane2
         )
     ], [], [
-        Aircraft("Boeing 707", "airbus", 20)
+        plane2
     ])
 ])
 
 def reserve():
-    print("Rezerwacja")
+    print("Wybierz lot")
+
+    flight = query_option_str(airport.get_flights(), True)
+    term = airport.get_terminal_of_flight(flight)
+    if not flight: return
+
+    pas = airport.query_passenger(term)
+    if not pas: return
+
+    seat = flight.query_reserve_seat()
+    if not seat: return
+    
+    print("Zarezerwowano pomyślnie:")
+    print("Pasażer:", pas)
+    print("Lot:", flight)
+    print("Samolot:", flight.aircraft)
 
 def main():
     print("Witaj w lotnisku")
