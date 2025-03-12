1729fbfd36823594c91df4333070b8832fe99a8e
Wolfyxon <wolfyxon@gmail.com>
2025-03-12 14:15:34 +0100

    Moved query_passenger() to Airport

--- highSchool/3/ProgramowanieObiektowe/05.03.2025/zadanie3/Airport.py
+++ highSchool/3/ProgramowanieObiektowe/05.03.2025/zadanie3/Airport.py
@@ -2,6 +2,7 @@ from Terminal import Terminal
 from Flight import Flight
 from Aircraft import Aircraft
 from Passenger import Passenger
+from cli import *
 
 class Airport:
     def __init__(self, terminals: list[Terminal]):
@@ -29,4 +30,29 @@ class Airport:
         for i in self.terminals:
             res += i.passengers
 
+        return res
+
+    def query_passenger(self) -> Passenger:
+        options = []
+
+        for i in self.get_passengers():
+            options.append(Option(str(i), i))
+
+        options.append(Option("< Nowy >", "new"))
+        res = query_option(options)
+
+        if not res:
+            return
+
+        if res == "new":
+            name1 = input("Podaj imię: ")
+            name2 = input("Podaj nazwisko: ")
+            doc_id = input_int("Podaj nr dokumentu: ")
+            ticket_class = input_int("Podaj klasę biletu: ")
+
+            p = Passenger(name1, name2, doc_id, ticket_class)
+            self.add_passenger(p)
+
+            return p
+        
         return res
