e585af3802b24f31263c1919efb91efca8716d2a
Wolfyxon <wolfyxon@gmail.com>
2025-03-12 14:23:14 +0100

    Moved seat logic to flight

--- highSchool/3/ProgramowanieObiektowe/05.03.2025/zadanie3/Flight.py
+++ highSchool/3/ProgramowanieObiektowe/05.03.2025/zadanie3/Flight.py
@@ -1,8 +1,21 @@
 from datetime import datetime
 
+class Seat:
+    def __init__(self, id: int):
+        self.id = id
+
 class Flight:
     def __init__(self, id: int, course: str, departure: datetime, arrival: datetime):
         self.id = id
         self.course = course
         self.departure = departure
         self.arrival = arrival
+        self.occupied_seats = []
+
+    def get_seat(self, id: int) -> Seat:
+        for i in self.occupied_seats:
+            if i.id == id:
+                return id
+
+    def get_available_seat_count(self) -> int:
+        return self.seat_count - len(self.occupied_seats)
