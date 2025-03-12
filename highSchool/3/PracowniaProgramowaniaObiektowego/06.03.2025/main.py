72f1821d3c64604fa752d7e709ef939bba89a9a5
Wolfyxon <wolfyxon@gmail.com>
2025-03-12 12:33:19 +0100

    Does that even work

--- highSchool/3/PracowniaProgramowaniaObiektowego/06.03.2025/main.py
+++ highSchool/3/PracowniaProgramowaniaObiektowego/06.03.2025/main.py
@@ -11,4 +11,20 @@ def bubble_sort(nums: list[float]) -> list[float]:
 
     return nums
 
+def select_sort(nums: list[float]) -> list[float]:
+    ln = len(nums) - 1
+
+    for i in range(ln):
+        min_i = 0
+
+        for j in range(i, ln):
+            if nums[j] < nums[min_i]:
+                min_i = j
+        print(min_i)
+        #nums[i], nums[min_i + 1] = nums[min_i + 1], nums[i]
+
+    return nums
+
+
 print(bubble_sort([2, 5, 3, 1]))
+print(select_sort([1, 9, 5, 3, 0, 1]))
