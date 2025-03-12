f9463d14e23291bbedaaeb13dc92722506dca88a
Wolfyxon <wolfyxon@gmail.com>
2025-03-12 12:49:46 +0100

    Init flights

--- highSchool/3/ProgramowanieObiektowe/05.03.2025/zadanie3/main.py
+++ highSchool/3/ProgramowanieObiektowe/05.03.2025/zadanie3/main.py
@@ -1,13 +1,26 @@
 from Airport import *
 from Terminal import *
 from cli import *
+from datetime import datetime
 
 
 airport = Airport([
-    Terminal([], [], [
+    Terminal([
+        Flight(
+            1234, "Warszawa -> Bruksela", 
+            datetime(2025, 6, 17, 11, 30), 
+            datetime(2025, 6, 17, 18, 40)
+        )
+    ], [], [
         Aircraft("Boeing 737", "airbus", 20)
     ]),
-    Terminal([], [], [
+    Terminal([
+            Flight(
+            4321, "Warszawa -> Waszyngton", 
+            datetime(2025, 4, 5, 15, 00), 
+            datetime(2025, 5, 5, 6, 10)
+        )
+    ], [], [
         Aircraft("Boeing 707", "airbus", 20)
     ])
 ])
