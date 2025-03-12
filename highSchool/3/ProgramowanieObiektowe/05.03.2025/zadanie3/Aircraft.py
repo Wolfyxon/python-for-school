f5b106de061f60f1b5247e7cd0aa2689455a2b90
Wolfyxon <wolfyxon@gmail.com>
2025-03-12 13:00:40 +0100

    Seat system

--- highSchool/3/ProgramowanieObiektowe/05.03.2025/zadanie3/Aircraft.py
+++ highSchool/3/ProgramowanieObiektowe/05.03.2025/zadanie3/Aircraft.py
@@ -1,5 +1,18 @@
+class Seat:
+    def __init__(self, id: int):
+        self.id = id
+
 class Aircraft:
-    def __init__(self, registration: str, aircraft_type: str, seats: int):
+    def __init__(self, registration: str, aircraft_type: str, seat_count: int):
         self.registration = registration
         self.aircraft_type = aircraft_type
-        self.seats = seats
+        self.seat_count = seat_count
+        self.occupied_seats = []
+
+    def get_seat(self, id: int) -> Seat:
+        for i in self.occupied_seats:
+            if i.id == id:
+                return id
+
+    def get_available_seat_count(self) -> int:
+        return seat_count - len(self.occupied_seats)
