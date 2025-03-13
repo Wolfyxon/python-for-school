f8357e5d5f52148fe6e518b0c611bc9ea4feebb1
Wolfyxon <wolfyxon@gmail.com>
2025-03-13 10:44:48 +0100

    max

--- highSchool/3/PracowniaProgramowaniaObiektowego/06.03.2025/main.py
+++ highSchool/3/PracowniaProgramowaniaObiektowego/06.03.2025/main.py
@@ -11,7 +11,7 @@ def bubble_sort(nums: list[float]) -> list[float]:
 
     return nums
 
-def select_sort(nums: list[float]) -> list[float]:
+def select_sort_min(nums: list[float]) -> list[float]:
     ln = len(nums)
 
     for i in range(ln):
@@ -25,6 +25,22 @@ def select_sort(nums: list[float]) -> list[float]:
 
     return nums
 
+def select_sort_max(nums: list[float]) -> list[float]:
+    ln = len(nums)
+
+    for i in range(ln):
+        max_i = i
+
+        for j in range(i + 1, ln):
+            if nums[j] > nums[max_i]:
+                max_i = j
+        
+        nums[i], nums[max_i] = nums[max_i], nums[i]
+
+    return nums
+
+
 
 print(bubble_sort([2, 5, 3, 1]))
-print(select_sort([1, 9, 5, 3, 0, 1]))
+print(select_sort_min([1, 9, 5, 3, 0, 1]))
+print(select_sort_max([1, 6, 3, 3, 2, 9]))
