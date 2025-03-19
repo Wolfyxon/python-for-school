1da8682572a41308f3d6664d4c000fc9d0009d8f
Wolfyxon <wolfyxon@gmail.com>
2025-03-19 08:22:42 +0100

    Gallery img switching

--- highSchool/3/JezykiProgramowania/19.03.2025/skrypt.js
+++ highSchool/3/JezykiProgramowania/19.03.2025/skrypt.js
@@ -3,5 +3,9 @@ window.addEventListener("load", () => {
     const mainImg = document.getElementById("main-img");
     const imgs = document.querySelectorAll("#gallery img");
 
-    
+    imgs.forEach((img) => {
+        img.addEventListener("click", () => {
+            mainImg.src = img.src;
+        })
+    });
 });
