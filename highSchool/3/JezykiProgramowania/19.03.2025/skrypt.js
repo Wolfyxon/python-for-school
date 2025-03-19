6c3faa6962aada6e5e48e0299c9220b682e44f33
Wolfyxon <wolfyxon@gmail.com>
2025-03-19 08:25:07 +0100

    Liking

--- highSchool/3/JezykiProgramowania/19.03.2025/skrypt.js
+++ highSchool/3/JezykiProgramowania/19.03.2025/skrypt.js
@@ -3,9 +3,21 @@ window.addEventListener("load", () => {
     const mainImg = document.getElementById("main-img");
     const imgs = document.querySelectorAll("#gallery img");
 
+    let isLiked = false;
+
     imgs.forEach((img) => {
         img.addEventListener("click", () => {
             mainImg.src = img.src;
         })
     });
+
+    likeImg.addEventListener("click", () => {
+        isLiked = !isLiked;
+        
+        if(isLiked) {
+            likeImg.src = "icon-on.svg";
+        } else {
+            likeImg.src = "icon-off.svg";
+        }
+    })
 });
