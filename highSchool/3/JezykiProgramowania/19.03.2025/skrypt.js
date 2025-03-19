93c5dccd75ee11742fb57dfe5af1f43b1a9a2ab9
Wolfyxon <wolfyxon@gmail.com>
2025-03-19 08:21:51 +0100

    Init script

---
+++ highSchool/3/JezykiProgramowania/19.03.2025/skrypt.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", () => {
+    const likeImg = document.getElementById("like");
+    const mainImg = document.getElementById("main-img");
+    const imgs = document.querySelectorAll("#gallery img");
+
+    
+});
