68e8ecd8c5189b451c028fd411e8922f805bb01c
Wolfyxon <wolfyxon@gmail.com>
2026-02-03 07:47:12 +0100

    Unit tests

--- highSchool/4/03.02.2026/main.py
+++ highSchool/4/03.02.2026/main.py
@@ -12,21 +12,45 @@ def process_array(array):
             for j in range(2 * i, num_count, i):
                 array[j] = False
 
-def print_array(array):
+def print_bool_array(array):
     for i in range(2, len(array)):
         if array[i]:
             print(i, end=" ")
     
     print()
 
+def bool_array_to_numbers(array):
+    res = []
+
+    for i in range(2, len(array)):
+        if array[i]:
+            res.append(i)
+
+    return res
+
+def test_array(length, expected):
+    array = []
+    fill_array(array, length)
+    process_array(array)
+
+    assert bool_array_to_numbers(array) == expected, f"Zwrócono {array}, oczekiwano {expected}"
+
+def run_tests():
+    test_array(5, [2, 3])
+    test_array(10, [2, 3, 5, 7])
+    test_array(20, [2, 3, 5, 7, 11, 13, 17, 19])
+
+    print("Testy zakończone pomyślnie")
+
 def main():
+    run_tests()
     num_count = int(input("Podaj ilość liczb: "))
 
-    numbers = []
-    fill_array(numbers, num_count)
-    process_array(numbers)
+    bools = []
+    fill_array(bools, num_count)
+    process_array(bools)
 
     print("Liczby pierwsze:")
-    print_array(numbers)
+    print_bool_array(bools)
 
 main()
